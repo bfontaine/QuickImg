0df18a0ed7f6f459de200efdc0f509bbfe97f29b
Baptiste Fontaine <batifon@yahoo.fr>
2012-10-09 23:54:51 +0200

    scope issue fixed.

--- quickimg.js
+++ quickimg.js
@@ -1,24 +1,26 @@
-(function(imgs, hd, undefined) {
+(function(imgs, hd) {
 
-    for (var i=0 , l=imgs.length,img; i<l; i++) {
-        img = imgs[i];
+    for (var i=0 , l=imgs.length; i<l; i++) {
+        (function(img){
 
-        if (!img.dataset[hd])
-            return;
+            if (!img.dataset[hd])
+                return;
 
-        var tmp_img = new Image(),
-            loaded  = false;
+            var tmp_img = new Image(),
+                loaded  = false;
 
-        tmp_img.onload = function() {
-            loaded = true; 
-            img.src = tmp_img.src;
-        };
+            tmp_img.onload = function() {
+                loaded = true; 
+                img.src = tmp_img.src;
+            };
 
-        tmp_img.src = img.dataset[hd];
-        delete img.dataset[hd];
+            tmp_img.src = img.dataset[hd];
+            delete img.dataset[hd];
 
-        if (!loaded && tmp_img.complete)
-            tmp_img.onload();
+            if (!loaded && tmp_img.complete)
+                tmp_img.onload();
+
+        })(imgs[i]);
     }
 
 })(document.getElementsByTagName('img'), 'hdSrc');
