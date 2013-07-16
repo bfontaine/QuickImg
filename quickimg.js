150cc21d92b8dd17557ab31c0a945b74fca267ae
Baptiste Fontaine <batifon@yahoo.fr>
2013-07-17 00:31:05 +0200

    Using .(get|remove)Attribute instead of .dataset for browsers that don't support the dataset API.

--- quickimg.js
+++ quickimg.js
@@ -1,21 +1,28 @@
-(function(imgs, hd) {
+;(function(imgs, hd_attr) {
+
+    'use strict';
 
     for (var i=0 , l=imgs.length; i<l; i++) {
         (function(img){
 
-            if (!img.dataset[hd])
+            // we use .(get|remove)Attribute for browsers
+            // that don't support the dataset API.
+            var hd_val = img.getAttribute(hd_attr),
+                loaded = false,
+                tmp_img;
+
+            if (!hd_val)
                 return;
 
-            var tmp_img = new Image(),
-                loaded  = false;
+            tmp_img = new Image();
 
             tmp_img.onload = function() {
                 loaded = true; 
                 img.src = tmp_img.src;
             };
 
-            tmp_img.src = img.dataset[hd];
-            delete img.dataset[hd];
+            tmp_img.src = hd_val;
+            img.removeAttribute(hd_attr);
 
             if (!loaded && tmp_img.complete)
                 tmp_img.onload();
@@ -23,4 +30,4 @@
         })(imgs[i]);
     }
 
-})(document.getElementsByTagName('img'), 'hdSrc');
+})(document.getElementsByTagName('img'), 'data-hd-src');
